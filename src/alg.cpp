1ae8fffd865d1cf0afa09eac76933e3bef212a4b
ZnakVoprosa1 <184nikiforov7b@mail.ru>
2025-03-01 23:19:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-
+while (b != 0) {
+        int temp = b;
+        b = a % b;  // Остаток от деления
+        a = temp;   // Обновляем a
+    }
+    return a; 
 }
