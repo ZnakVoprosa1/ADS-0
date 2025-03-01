8e1abac38bf25b18d96ad7e1d4a24fa3e28943a7
Степан <184nikiforov7b@mail.ru>
2025-03-01 23:41:05 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,7 @@
 
 int gcd(int a, int b) {
 while (b != 0) {
-        int temp = b;
-        b = a % b;  // Остаток от деления
-        a = temp;   // Обновляем a
-    }
-    return a; 
-}
+int temp = b;
+b = a % b;
+a = temp;}
+return a;}
